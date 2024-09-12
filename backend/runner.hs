5aee266d744718d31b8d1caf08d1b87d30395962
Nahum Murillo <n4hummuro227@gmail.com>
2024-09-12 01:13:10 -0600

    Changes (Structure Ideas)

---
+++ backend/runner.hs
@@ -0,0 +1,11 @@
+--generate randomly tanks positions function
+--- HERE
+---
+--- HERE
+
+
+--- Verify position of tanks if they are not in the same position or in the same position of the player or obstacles
+--- HERE
+---
+--- HERE
+
